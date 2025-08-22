0795edae040e2ef3f39ab7b5bf3a96797d71d62f
Anas123-svg <bscs23190@itu.edu.pk>
2025-08-23 04:53:11 +0500

    update

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\ContactMail;
 
+//test
 class ContactController extends Controller
 {
     public function store(Request $request)
