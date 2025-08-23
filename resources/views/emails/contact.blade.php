c9458df2ec58b7e387325740b03b5952c9b682b0
Anas123-svg <bscs23190@itu.edu.pk>
2025-08-23 05:24:21 +0500

    add

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,18 +1,34 @@
 @component('mail::message')
-# New Contact Message
+<div style="font-family: 'Arial', sans-serif; color: #333; line-height: 1.6; padding: 10px;">
+    <h2 style="color: #E94E77;">New Contact Message</h2>
 
-**Name:** {{ $data['name'] }}  
-**Email:** {{ $data['email'] }}  
-@if(!empty($data['whatsapp']))
-**WhatsApp:** {{ $data['whatsapp'] }}  
-@endif
-@if(!empty($data['phone']))
-**Phone:** {{ $data['phone'] }}  
-@endif
+    <table style="width: 100%; border-collapse: collapse; margin: 10px 0;">
+        <tr>
+            <td style="font-weight: bold; padding: 5px; width: 120px;">Name:</td>
+            <td style="padding: 5px;">{{ $data['name'] }}</td>
+        </tr>
+        <tr>
+            <td style="font-weight: bold; padding: 5px;">Email:</td>
+            <td style="padding: 5px;">{{ $data['email'] }}</td>
+        </tr>
+        @if(!empty($data['whatsapp']))
+        <tr>
+            <td style="font-weight: bold; padding: 5px;">WhatsApp:</td>
+            <td style="padding: 5px;">{{ $data['whatsapp'] }}</td>
+        </tr>
+        @endif
+        @if(!empty($data['phone']))
+        <tr>
+            <td style="font-weight: bold; padding: 5px;">Phone:</td>
+            <td style="padding: 5px;">{{ $data['phone'] }}</td>
+        </tr>
+        @endif
+        <tr>
+            <td style="font-weight: bold; padding: 5px; vertical-align: top;">Message:</td>
+            <td style="padding: 5px;">{{ $data['message'] }}</td>
+        </tr>
+    </table>
 
-**Message:**
-{{ $data['message'] }}
-
-Thanks,  
-{{ config('app.name') }}
+    <p style="font-size: 16px;">Thanks,<br><strong>{{ config('app.name') }}</strong></p>
+</div>
 @endcomponent
--- resources/views/emails/contact_confirmation.blade.php
+++ resources/views/emails/contact_confirmation.blade.php
@@ -1,11 +1,16 @@
 @component('mail::message')
-# Hello {{ $data['name'] }},
+<div style="font-family: 'Arial', sans-serif; color: #333; line-height: 1.6; padding: 10px;">
+    <h2 style="color: #4A90E2;">Hello {{ $data['name'] }},</h2>
 
-We have received your message and will be in touch with you soon.
+    <p style="font-size: 16px;">
+        We have received your message and will be in touch with you soon.
+    </p>
 
-**Your Message:**  
-{{ $data['message'] }}
+    <div style="background-color: #f7f7f7; padding: 15px; border-left: 4px solid #4A90E2; margin: 15px 0; border-radius: 5px;">
+        <strong>Your Message:</strong><br>
+        <p style="margin: 5px 0;">{{ $data['message'] }}</p>
+    </div>
 
-Thanks,<br>
-{{ config('app.name') }}
+    <p style="font-size: 16px;">Thanks,<br><strong>{{ config('app.name') }}</strong></p>
+</div>
 @endcomponent
