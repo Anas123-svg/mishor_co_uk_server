c276a3a870b75b5ae1e4accdc8b2b425a2672ae5
Anas123-svg <bscs23190@itu.edu.pk>
2025-08-27 15:25:09 +0500

    update

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,8 +9,10 @@ use App\Mail\ContactConfirmationMail;
 
 class ContactController extends Controller
 {
-    public function store(Request $request)
-    {
+public function store(Request $request)
+{
+    try {
+        // Validate request
         $validated = $request->validate([
             'name'            => 'required|string|max:255',
             'email'           => 'required|email',
@@ -20,17 +22,36 @@ class ContactController extends Controller
             'location'        => 'nullable|string|max:255',
             'project_details' => 'nullable|string',
             'images'          => 'nullable|array',
-            'images.*'        => 'string', 
+            'images.*'        => 'string',
         ]);
 
+        // Send confirmation email to user
         Mail::to($validated['email'])->send(new ContactConfirmationMail($validated));
 
+        // Send original message to admins
         Mail::to(['m.anastebrw2@gmail.com'])->send(new ContactMail($validated));
 
         return response()->json([
             'success' => true,
             'message' => 'Message sent successfully!'
-        ]);
+        ], 200);
+
+    } catch (\Illuminate\Validation\ValidationException $e) {
+        // Validation errors
+        return response()->json([
+            'success' => false,
+            'errors' => $e->errors()
+        ], 422);
+
+    } catch (\Exception $e) {
+        // General errors
+        return response()->json([
+            'success' => false,
+            'message' => 'Something went wrong while sending your message.',
+            'error' => $e->getMessage()
+        ], 500);
     }
+}
+
 }
 
